cc3e12a1368d4435f24d0c7703a8c6bdc408ddd2
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2023-05-16 12:11:14 +0430

    i wrote test for subtact function then wrote subtract function in app.calc

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,3 +5,7 @@ class CalcTests (SimpleTestCase):
     def test_add_numbers (self):
         res = calc.add(10, 5)
         self.assertEqual(res, 15)
+
+    def test_subtract_number (self):
+        res = calc.subtract(15, 10)
+        self.assertEqual(res, 5)
