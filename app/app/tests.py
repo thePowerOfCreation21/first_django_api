c916fd70fbdbaff3b7d8b86091b196991ab72509
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2023-05-17 09:59:33 +0430

    configured postgresql on docker-compose

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,11 +1,12 @@
 from django.test import SimpleTestCase
 from app import calc
 
-class CalcTests (SimpleTestCase):
-    def test_add_numbers (self):
+
+class CalcTests(SimpleTestCase):
+    def test_add_numbers(self):
         res = calc.add(10, 5)
         self.assertEqual(res, 15)
 
-    def test_subtract_number (self):
+    def test_subtract_number(self):
         res = calc.subtract(15, 10)
         self.assertEqual(res, 5)
