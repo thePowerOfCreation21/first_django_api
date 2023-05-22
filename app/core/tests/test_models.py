c97dec193402649705878304e7b3d3de2326bb49
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2023-05-22 12:14:29 +0430

    implemented test_new_user_without_email_raises_error

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -35,3 +35,8 @@ class ModelTests(TestCase):
         for email, expected_email in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected_email)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises ValueError."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'sample123')
