b94e2b70767555c14587220ecb98f51f5dd6ec67
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2023-07-09 17:59:30 +0330

    implemented get recipe detail API.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -9,7 +9,7 @@ from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """view for manage Recipe APIs."""
-    serializer_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -17,3 +17,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Retrieve recipes for authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        """return the serializer class for request."""
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+        return self.serializer_class
