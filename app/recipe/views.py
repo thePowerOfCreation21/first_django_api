1dd660221f770342ed08b0f50ecb7c68a9db107d
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2023-07-09 13:18:45 +0330

    implemented APIs for recipe

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render # noqa
+"""views for Recipe APIs."""
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from core.models import Recipe
+from recipe import serializers
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """view for manage Recipe APIs."""
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve recipes for authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
